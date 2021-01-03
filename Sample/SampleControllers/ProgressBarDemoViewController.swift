fb1ae75e470c42e9a57521cc8b663effd40329be
Eagle Diao <eaglediao@gmail.com>
2021-01-03 18:37:14 -0500

    progress ring

--- Sample/SampleControllers/ProgressBarDemoViewController.swift
+++ Sample/SampleControllers/ProgressBarDemoViewController.swift
@@ -4,6 +4,9 @@ import UIKit
 class ProgressBarDemoViewController: BaseScrollableDemoViewController {
 
     var linearProgressBar1 = LinearProgressBar()
+    var progressRing1 = ProgressRing()
+    var progressRing2 = ProgressRing()
+    var progressRing3 = ProgressRing()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,24 +41,76 @@ class ProgressBarDemoViewController: BaseScrollableDemoViewController {
             }
         )
 
+        contentLinearLayout.addSubview(FrameLayout().apply {
+            $0.marginTop = Dimens.marginMedium
+            $0.layoutGravity = .centerHorizontal
+            $0.addSubview(
+                progressRing1.apply {
+                    $0.layoutSize = CGSize(width: dp(128), height: dp(128))
+                    $0.addProgress(key: "main",
+                                   progress: 1.0,
+                                   color: .red)
+                    $0.addProgress(key: "sub",
+                                   progress: 1.0,
+                                   color: .green)
+                }
+            )
+            $0.addSubview(
+                progressRing2.apply {
+                    $0.padding = dp(8)
+                    $0.layoutSize = CGSize(width: dp(128), height: dp(128))
+
+                    $0.addProgress(key: "main",
+                                   progress: 1.0,
+                                   color: .cyan)
+                    $0.addProgress(key: "sub",
+                                   progress: 1.0,
+                                   color: .purple)
+                }
+            )
+            $0.addSubview(
+                progressRing3.apply {
+                    $0.padding = dp(16)
+                    $0.layoutSize = CGSize(width: dp(128), height: dp(128))
+
+                    $0.addProgress(key: "main",
+                                   progress: 1.0,
+                                   color: .gray)
+                    $0.addProgress(key: "sub",
+                                   progress: 1.0,
+                                   color: .darkGray)
+                }
+            )
+        })
+
         contentLinearLayout.addSubview(
             EDButton().apply {
+                $0.marginTop = Dimens.marginMedium
                 $0.layoutGravity = [.fillHorizontal]
                 $0.setTitle("Random Progress", for: .normal)
                 $0.setTitleColor(.blue, for: .normal)
                 $0.addTarget(self, action: #selector(randomButtonPressed), for: .touchUpInside)
             }
         )
+
     }
 
     @objc private func randomButtonPressed() {
-        self.linearProgressBar1.updateProgress(
-            key: "main",
-            progress: CGFloat(Float.random(min: 0, max: 1))
-        )
-        self.linearProgressBar1.updateProgress(
-            key: "sub",
-            progress: CGFloat(Float.random(min: 0, max: 1))
-        )
+        linearProgressBar1.also {
+            $0.updateProgress(key: "main", progress: CGFloat(Float.random(min: 0, max: 1)))
+            $0.updateProgress(key: "sub", progress: CGFloat(Float.random(min: 0, max: 1)))
+        }
+        progressRing1.also {
+            $0.updateProgress(key: "main", progress: CGFloat(Float.random(min: 0, max: 1)))
+            $0.updateProgress(key: "sub", progress: CGFloat(Float.random(min: 0, max: 1)))
+        }
+        progressRing2.also {
+            $0.updateProgress(key: "main", progress: CGFloat(Float.random(min: 0, max: 1)))
+            $0.updateProgress(key: "sub", progress: CGFloat(Float.random(min: 0, max: 1)))
+        }
+        progressRing3.also {
+            $0.updateProgress(key: "main", progress: CGFloat(Float.random(min: 0, max: 1)))
+            $0.updateProgress(key: "sub", progress: CGFloat(Float.random(min: 0, max: 1)))
+        }
     }
 }
---
+++ oneHookLibrary/UI/Views/ProgressRing.swift
@@ -0,0 +1,109 @@
+import UIKit
+
+open class ProgressRing: BaseView {
+
+    private struct LayerInfo {
+        var progress: CGFloat
+        var color: UIColor
+    }
+
+    private var progressLayers = [String: CAShapeLayer]()
+    private var layerInfos = [String: LayerInfo]()
+    public var thickness: CGFloat = dp(8)
+
+    open override func commonInit() {
+        super.commonInit()
+        self.layoutSize = CGSize(width: dp(32), height: dp(32))
+
+    }
+
+    deinit {
+        layer.sublayers?.forEach {
+            $0.removeFromSuperlayer()
+        }
+    }
+
+    open override func sizeThatFits(_ size: CGSize) -> CGSize {
+        layoutSize
+    }
+
+    public func addProgress(key: String,
+                            progress: CGFloat = 0,
+                            color: UIColor) {
+        guard layerInfos[key] == nil else {
+            fatalError("Progress layer with \(key) already exist")
+        }
+
+        let progressLayer = CAShapeLayer()
+        progressLayer.anchorPoint = CGPoint(x: 0, y: 0)
+        let layerInfo = LayerInfo(
+            progress: progress,
+            color: color
+        )
+        progressLayers[key] = progressLayer
+        layerInfos[key] = layerInfo
+        layer.addSublayer(progressLayer)
+
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        invalidateLayer(layer: progressLayer, info: layerInfo)
+        CATransaction.commit()
+    }
+
+    public func updateProgress(key: String,
+                               progress: CGFloat,
+                               color: UIColor? = nil,
+                               animated: Bool = true) {
+        guard
+            let progressLayer = progressLayers[key],
+            let layerInfo = layerInfos[key] else {
+            fatalError("Progress layer with \(key) does not exist")
+        }
+
+        let newLayerInfo = LayerInfo(
+            progress: progress,
+            color: color ?? layerInfo.color
+        )
+        layerInfos[key] = newLayerInfo
+
+        if !animated {
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+        }
+        invalidateLayer(layer: progressLayer, info: newLayerInfo)
+        if !animated {
+            CATransaction.commit()
+        }
+    }
+
+    open override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        for element in layerInfos {
+            if
+                let layer = progressLayers[element.key],
+                let info = layerInfos[element.key] {
+                invalidateLayer(layer: layer, info: info)
+            }
+        }
+    }
+
+    private func invalidateLayer(layer: CAShapeLayer, info: LayerInfo) {
+        let length = bounds.width - paddingStart - paddingEnd - thickness
+        let frame = CGRect(x: paddingStart + thickness / 2,
+                           y: paddingTop + thickness / 2,
+                           width: length,
+                           height: length)
+        if layer.bounds != frame {
+            layer.bounds = frame
+            layer.position = frame.origin
+            layer.path = UIBezierPath(ovalIn: frame).cgPath
+        }
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = info.color.cgColor
+        layer.lineWidth = thickness
+        layer.strokeStart = 0
+        layer.strokeEnd = info.progress
+        layer.lineCap = .round
+    }
+
+}
