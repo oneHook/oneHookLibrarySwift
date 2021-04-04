d00baf228326837d7a1d12ff62e528256eb0a3a7
Eagle Diao <>
2021-04-03 20:26:10 -0400

    update row layout

--- oneHookLibrary/UI/Views/Layout/RowLayout.swift
+++ oneHookLibrary/UI/Views/Layout/RowLayout.swift
@@ -2,7 +2,7 @@ import UIKit
 
 open class RowLayout: BaseView {
 
-    var spacing: CGFloat = 0
+    public var spacing: CGFloat = 0
 
     open override func layoutSubviews() {
         super.layoutSubviews()
@@ -10,26 +10,34 @@ open class RowLayout: BaseView {
             return
         }
         let childCount = CGFloat(subviews.count)
-        let childWidth = (bounds.width - spacing * (childCount - 1)) / childCount
+        let childWidth = (bounds.width - paddingStart - paddingEnd - spacing * (childCount - 1)) / childCount
         for index in subviews.indices {
             let child = subviews[index]
-            let currX = CGFloat(index) * (childWidth + spacing)
-            child.frame = CGRect(x: currX, y: 0, width: childWidth, height: bounds.height)
+            let currX = paddingStart + CGFloat(index) * (childWidth + spacing)
+            child.frame = CGRect(
+                x: currX,
+                y: paddingTop,
+                width: childWidth,
+                height: bounds.height - paddingTop - paddingBottom
+            )
         }
     }
 
     open override func sizeThatFits(_ size: CGSize) -> CGSize {
+        guard layoutSize != .zero else {
+            return layoutSize
+        }
         guard subviews.isNotEmpty else {
             return .zero
         }
         let childCount = CGFloat(subviews.count)
-        let childWidth = (size.width - spacing * (childCount - 1)) / childCount
+        let childWidth = (size.width - paddingStart - paddingEnd - spacing * (childCount - 1)) / childCount
         var maxHeight = CGFloat(0)
         for index in subviews.indices {
             let child = subviews[index]
             let childSize = child.sizeThatFits(CGSize(width: childWidth, height: CGFloat.greatestFiniteMagnitude))
             maxHeight = max(childSize.height, maxHeight)
         }
-        return CGSize(width: size.width, height: maxHeight)
+        return CGSize(width: size.width, height: maxHeight + paddingTop + paddingBottom)
     }
 }
