7068c5e80fa9d1dc0957caa4415ceb610bcadb3f
Eagle Diao <eaglediao_home@Eagles-iMac.local>
2021-04-14 11:33:01 -0400

    fix toolbar memory leak

--- oneHookLibrary/UI/Views/EDToolbar.swift
+++ oneHookLibrary/UI/Views/EDToolbar.swift
@@ -9,87 +9,87 @@ public class EDToolbar: BaseView {
         layout.backgroundColor = .clear
     }
 
-    private lazy var centerContainer = optionalBuilder {
+    private lazy var centerContainer = optionalBuilder { [weak self] in
         LinearLayout().apply {
             $0.orientation = .horizontal
             $0.layoutGravity = .center
             $0.marginStart = dp(64)
             $0.marginEnd = dp(64)
-            self.contentContainer.addSubview($0)
+            self?.contentContainer.addSubview($0)
         }
     }
 
-    private lazy var labelContainer = optionalBuilder {
+    private lazy var labelContainer = optionalBuilder { [weak self] in
         LinearLayout().apply {
             $0.orientation = .vertical
             $0.layoutGravity = .centerVertical
-            self.centerContainer.getOrMake().addSubview($0)
+            self?.centerContainer.getOrMake().addSubview($0)
         }
     }
 
-    public lazy var centerImageView = optionalBuilder {
+    public lazy var centerImageView = optionalBuilder { [weak self] in
         EDImageView().apply {
             $0.layoutGravity = .centerVertical
-            self.centerContainer.getOrMake().insertSubview($0, at: 0)
+            self?.centerContainer.getOrMake().insertSubview($0, at: 0)
             $0.marginEnd = Dimens.marginMedium
         }
     }
 
-    public lazy var centerLabel = optionalBuilder {
+    public lazy var centerLabel = optionalBuilder { [weak self] in
         EDLabel().apply {
             $0.layoutGravity = .centerHorizontal
             $0.font = Fonts.bold(Fonts.fontSizeMedium)
             $0.textAlignment = .center
             $0.textColor = .ed_toolbarTextColor
-            self.labelContainer.getOrMake().addSubview($0)
+            self?.labelContainer.getOrMake().addSubview($0)
         }
     }
 
-    public lazy var centerSubtitleLabel = optionalBuilder {
+    public lazy var centerSubtitleLabel = optionalBuilder { [weak self] in
         EDLabel().apply {
             $0.layoutGravity = .centerHorizontal
             $0.font = Fonts.regular(Fonts.fontSizeMedium)
             $0.textAlignment = .center
             $0.textColor = .ed_toolbarTextColor
             $0.marginTop = dp(4)
-            self.labelContainer.getOrMake().addSubview($0)
+            self?.labelContainer.getOrMake().addSubview($0)
         }
     }
 
-    public lazy var leftLabel = optionalBuilder {
+    public lazy var leftLabel = optionalBuilder { [weak self] in
         EDLabel().apply({ (label) in
             label.font = Fonts.bold(Fonts.fontSizeMedium)
             label.textColor = .ed_toolbarTextColor
             label.layoutGravity = [.centerVertical, .start]
             label.marginStart = Dimens.marginMedium
             label.textAlignment = .left
-            self.contentContainer.addSubview(label)
+            self?.contentContainer.addSubview(label)
         })
     }
 
-    public lazy var leftNavigationButton = optionalBuilder({
+    public lazy var leftNavigationButton = optionalBuilder({  [weak self] in
             ButtonGenerator.navigationButton().apply({ (button) in
                 button.marginStart = dp(9)
                 button.layoutGravity = [.centerVertical, .start]
-                self.contentContainer.addSubview(button)
+                self?.contentContainer.addSubview(button)
             })
         }, clearer: { (button) in
             button.removeFromSuperview()
         })
 
-    public lazy var rightNavigationButton = optionalBuilder {
+    public lazy var rightNavigationButton = optionalBuilder {  [weak self] in
         ButtonGenerator.navigationButton().apply({ (button) in
             button.marginEnd = Dimens.marginMedium
             button.layoutGravity = [.centerVertical, .end]
-            self.contentContainer.addSubview(button)
+            self?.contentContainer.addSubview(button)
         })
     }
 
-    public lazy var divider = optionalBuilder {
+    public lazy var divider = optionalBuilder {  [weak self] in
         ViewGenerator.divider().apply({ (divider) in
             divider.layoutGravity = [.bottom, .fillHorizontal]
             divider.layoutSize = CGSize(width: dp(1), height: dp(1))
-            self.contentContainer.addSubview(divider)
+            self?.contentContainer.addSubview(divider)
         })
     }
 
